709409dc191e19179a7799bf26bd26a1a106a3b3
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-02-17 19:37:40 -0300

    Dice Scene and Dice Logic

---
+++ Scenes/Dice/Dice.gd
@@ -0,0 +1,31 @@
+extends Area2D
+
+
+class_name Dice
+
+
+# Constants
+const SPEED = 100.0
+const ROTATION_SPEED = 100.0
+
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+# the direction of the dice is random
+var rotation_dir: float = 1.0
+
+
+func _ready() -> void:
+	# randf give two random numbers between 1.0 and 0.0 for default
+	if randf() < 0.5: rotation_dir *= -1
+	
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+	
+	
+func _physics_process(delta: float) -> void:
+	# This logic makes the dice fall up
+	position.y += SPEED * delta
+	sprite_2d.rotate(ROTATION_SPEED * rotation_dir)
