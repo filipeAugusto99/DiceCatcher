44fc24fceb5f073c25e696cfadcdb66b1d821f56
filipeAugusto99 <filipecordeiro213@gmail.com>
2026-02-19 19:46:22 -0300

    Groups and Fox Scene

---
+++ Scenes/Fox/Fox.gd
@@ -0,0 +1,27 @@
+extends Area2D
+
+
+class_name Fox
+
+
+@export var speed: float = 200.0
+
+
+@onready var sprite_2d: Sprite2D = $Sprite2D
+
+
+func _physics_process(delta: float) -> void:
+	var move: float = Input.get_axis("ui_left", "ui_right")
+	
+	#if Input.is_action_pressed("ui_left"):
+		#sprite_2d.flip_h = false
+		##move -= speed	
+	#if Input.is_action_pressed("ui_right"):
+		#sprite_2d.flip_h = true
+		##move += speed
+		#
+	
+	if !is_zero_approx(move):
+		sprite_2d.flip_h = move > 0.0
+		
+	position.x += move * delta * speed
--- Scenes/Game/Game.gd
+++ Scenes/Game/Game.gd
@@ -6,6 +6,10 @@ class_name Game
 
 const DICE = preload("uid://dqkcwymqyymme")
 const MARGIN: float = 80.0
+const STOPPABLE_GROUP: String = "stoppable"
+
+
+@onready var spawn_timer: Timer = $SpawnTimer
 
 
 static var _vp_r: Rect2
@@ -14,8 +18,15 @@ static var _vp_r: Rect2
 static func get_vpr() -> Rect2:
 	return _vp_r
 
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event.is_action_pressed("restart"):
+		get_tree().reload_current_scene()
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	spawn_dices()
 	update_vp()
 
 
@@ -32,8 +43,8 @@ func spawn_dices() -> void:
 	var new_dice: myDice = DICE.instantiate()
 	
 	var pos_x = randf_range(
-		_vp_r.position.x + MARGIN,
-		_vp_r.end.x - MARGIN
+		_vp_r.position.x,
+		_vp_r.end.x
 	)
 	
 	new_dice.position = Vector2(pos_x, -MARGIN)
@@ -42,9 +53,16 @@ func spawn_dices() -> void:
 	add_child(new_dice)
 	
 
-func _on_dice_game_over() -> void:
-	print("Game Over")
+func pause_all() -> void:
+	spawn_timer.stop()
+	var to_stop: Array[Node] = get_tree().get_nodes_in_group(STOPPABLE_GROUP)
+	for item in to_stop:
+		item.set_physics_process(false)
 
+	
+func _on_dice_game_over() -> void:
+	pause_all()
+	
 
 func _on_spawn_timer_timeout() -> void:
 	spawn_dices()
